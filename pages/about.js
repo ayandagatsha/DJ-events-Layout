e9ab6581121404eea59bbb662ac22471ef0e498c
Ayanda <kaygatsaha@gmail.com>
2021-10-21 07:36:38 +0200

    Pages and routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>An app to find the latest DJ and other musical events</p>
+            <p>
+                version 1
+            </p>
+        </div>
+    )
+}
